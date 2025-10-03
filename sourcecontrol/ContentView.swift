8b588e89f644300baf2f72774da73bb5b6c0deb0
Humera <khanhumerashabin272@gmail.com>
2025-10-03 19:27:25 +0530

    [patch] changed the button label to click here (was click me)

--- sourcecontrol/ContentView.swift
+++ sourcecontrol/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
-            Button("click me"){}
+            Button("click here"){}
         }
         .padding()
     }
