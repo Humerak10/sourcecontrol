61da3eff7dd259cf7286e2129c6e5e3e931eef44
Humera <khanhumerashabin272@gmail.com>
2025-10-03 22:16:24 +0530

    text from f1 branch

--- sourcecontrol/ContentView.swift
+++ sourcecontrol/ContentView.swift
@@ -34,6 +34,7 @@ struct ContentView: View {
             Text("Hello, world!")
             Button(" here"){}
             Button("click "){}
+            Text("add features")
         }
         .padding()
         
