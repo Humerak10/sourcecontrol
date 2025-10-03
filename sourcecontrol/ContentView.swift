f0a4c8fe05da3e1dbf1023d5051e965490ae8606
Humera <khanhumerashabin272@gmail.com>
2025-10-03 19:34:43 +0530

    [clean] added commit documentation

--- sourcecontrol/ContentView.swift
+++ sourcecontrol/ContentView.swift
@@ -4,6 +4,24 @@
 //
 //  Created by Humera Khan on 02/10/25.
 //
+/*
+ commit messages
+ 
+ new feature:
+ [feature] Description of feature
+ 
+ bug not in production:
+ [patch] Description of bug
+ 
+ bug in production:
+ [bug] Description of bug
+ 
+ mundane tasks:
+ [clean] Description of changes
+ 
+ release:
+ [release] Description of release
+ */
 
 import SwiftUI
 
